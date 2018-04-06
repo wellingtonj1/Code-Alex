8dd69a3f0f56d790d9380f124aeb6fb8a1f6975d
JapaBits <34574051+wellingtonj1@users.noreply.github.com>
2018-04-06 10:56:42 -0300

    Update Class (model) naturals.h

--- Class (model) naturals.h
+++ Class (model) naturals.h
@@ -1,6 +1,7 @@
-class naturais
+class naturals
 {
 private:
+
 int numN;
 
     public:
@@ -8,23 +9,14 @@ int numN;
     void setnatural(int n);
     int getnatural();
     void fatorialN();
-    int getfatorialN();
     void perfeitoN();
-    int getperfeitoN();
     void capicuaN();
-    inr getcapicuaN().
     void quadrapfN();
-    int getquadrapfN();
     void primoN();
-    int getprimoN();
     void mudabaseN();
-    int getmudabaseN();
     void maxdivN();
-    int getmaxdivN();
     void mindivN();
-    int getmindivN();
     void primo2N();
-    int getprimo2N();
-
-
+ 
+ 
 };
