fafa89b1cc727adf8746496544fa5ef87362e66a
JapaBits <34574051+wellingtonj1@users.noreply.github.com>
2018-04-11 14:01:51 -0300

    Update Tentativa de Reorganização.cpp

--- Tentativa de Reorganização.cpp
+++ Tentativa de Reorganização.cpp
@@ -1,9 +1,9 @@
 void natural(int n)
 {
 	if(n<0)
-	{	 
+	{
 	cout<< "\n você não digitou um numero natural : " << endl;
-	}	
+	}
 	else
 	{
 	numN=n;
@@ -20,22 +20,35 @@ int fatorialN()
 }
 
 bool perfeitoN()
-{	
+{
 	int raizn,soma=0;
 	raizn=sqrt(numN);
 	for(int i=1;i<raizn;i++)
 	{
 		if(numN%i==0)
-		soma+=i+numN/i;
+		soma+=i+(numN/i);
 	}
-	
+
 	return (numN==soma);
 }
     void capicuaN();
     void quadrapfN();
     bool primoN()
     {
-		if(numN%numN ==0 && numN/1 ==0) 
+        int raiz=0;
+        raiz=sqrt(numN);
+		for(int i=2; i<=numN; i++)
+		{
+            if(numN%!=0)
+            {
+            return true; //é primo
+            }
+            else
+            {
+            return false;
+            break;
+            }
+		}
 	}
     void mudabaseN();
     void maxdivN();
