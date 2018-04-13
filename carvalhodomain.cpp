39bb77956d82f6960effc917e282bb04547739d6
JapaBits <34574051+wellingtonj1@users.noreply.github.com>
2018-04-13 10:43:44 -0300

    Create carvalhodomain.cpp

---
+++ carvalhodomain.cpp
@@ -0,0 +1,15 @@
+#include "naturals.h"
+#include "naturals.cpp"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numero;
+    naturals p1;
+    cin>>numero;
+    p1.natural(numero);
+
+
+}
