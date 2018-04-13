6313c346cdcfa1580ce016e71cb1660686de54dd
JapaBits <34574051+wellingtonj1@users.noreply.github.com>
2018-04-13 12:06:26 -0300

    Update carvalhodomain.cpp

--- carvalhodomain.cpp
+++ carvalhodomain.cpp
@@ -8,8 +8,19 @@ int main()
 {
     int numero;
     naturals p1;
+    cout<<"\nDigite um numero natural :";
     cin>>numero;
+
     p1.natural(numero);
-
-
+    //cout<< p1.fatorialN();
+    if(p1.perfeitoN())
+    {
+        cout <<"\n é perfeito ";
+    }
+    else
+    {
+        cout <<"\n não é perfeito";
+    }
 }
+
+
