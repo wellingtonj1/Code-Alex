9f94b024b77a3d5ee6f8d07191b924988d313cbb
JapaBits <34574051+wellingtonj1@users.noreply.github.com>
2018-04-16 19:17:52 -0300

    Update carvalhodomainyasuo.cpp

--- carvalhodomainyasuo.cpp
+++ carvalhodomainyasuo.cpp
@@ -6,44 +6,79 @@ using namespace std;
 
 int main()
 {
-    int numero;
+    int numero,escolha;
     naturals p1;
     cout<<"\nDigite um numero natural :";
-    cin>>numero;
 
-    p1.natural(numero);
-    cout<< "\n O fatorial do numero " << numero << " é == " << p1.fatorialN();
-    if(p1.perfeitoN())
+    for(int i=1;i<=1;i++)
+        {
+            cin>>numero;
+            if(p1.natural(numero))
+            {// o numero é natural
+            }
+            else
+            { // nao e natural
+            cout<< "\n o numero digitado nao é natural... Digite novamente : " <<endl;
+            i=0;
+            }
+        }
+
+
+    cout<< "\n===========================================Qual operação desejas realizar ?=========================================== \n";
+
+    cout<< "\n Digite 1 para Calcular seu fatorial";
+    cout<< "\n Digite 2 para Verificar se é um numero perfeito";
+    cout<< "\n Digite 3 para Verificar se o numero é capicua";
+    cout<< "\n Digite 4 para Verificar se o numero é um quadrado perfeito"<<endl;
+    cin>>escolha;
+
+    switch(escolha)
     {
+
+        case 1:
+        {
+        cout<< "\n O fatorial do numero " << numero << " é == " << p1.fatorialN();
+        break;
+        }
+
+        case 2:
+        {
+        if(p1.perfeitoN())
+        {
         cout <<"\n o numero " << numero << " é perfeito \n";
-    }
-    else
-    {
+        }
+        else
+        {
         cout <<"\n o numero " << numero << " não é perfeito \n";
-    }
+        }
+        break;
+        }
 
-    // Capicua
-
-    if(p1.capicuaN())
-    {
+        case 3:
+        {
+        if(p1.capicuaN())
+        {
         cout<<"\n o numero " << numero << " é capicua \n";
-    }
-    else
-    {
+        }
+        else
+        {
         cout<<"\n o numero " << numero << " não é capicua \n";
-    }
+        }
+        break;
+        }
 
-    //Quadra perfect
-
-    if(p1.quadrapfN())
-    {
+        case 4:
+        {
+        if(p1.quadrapfN())
+        {
         cout<<"\n o numero " << numero << " é um quadrado perfeito \n";
-    }
-    else
-    {
+        }
+        else
+        {
         cout<<"\n o numero " << numero << " não é um quadrado perfeito \n";
+        }
+        break;
+        }
     }
-
-    //
 }
 
